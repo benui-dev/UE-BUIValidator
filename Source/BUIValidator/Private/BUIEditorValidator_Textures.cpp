8497f49bd1491080a8aeff1af428c81cba520aa9
ben <ben@benui.ca>
2021-02-28 22:00:43 -0800

    Update BUIEditorValidator_Textures.cpp

--- Source/BUIValidator/Private/BUIEditorValidator_Textures.cpp
+++ Source/BUIValidator/Private/BUIEditorValidator_Textures.cpp
@@ -44,10 +44,16 @@ EDataValidationResult UBUIEditorValidator_Textures::ValidateLoadedAsset_Implemen
 	UTexture2D* Texture = Cast<UTexture2D>( InAsset );
 	if ( Texture )
 	{
+		bool bAnyFailed = false;
 		if ( Texture->GetSizeX() % 4 != 0
 			|| Texture->GetSizeY() % 4 != 0 )
 		{
-			ValidationErrors.Add( FText::FromString( FString::Printf( TEXT( "Texture size must be a multiple of 4. Size is %d x %d." ), Texture->GetSizeX(), Texture->GetSizeY() ) ) );
+			//ValidationErrors.Add( FText::FromString( FString::Printf( TEXT( "Texture size must be a multiple of 4. Size is %d x %d." ), Texture->GetSizeX(), Texture->GetSizeY() ) ) );
+			bAnyFailed = true;
+			AssetFails( InAsset, FText::Format(
+				LOCTEXT( "BUIValidatorError_MultipleOfFour", "Texture asset must be a multiple of 4, but size is {0} x {1}" ),
+				FText::AsNumber( Texture->GetSizeX() ), FText::AsNumber( Texture->GetSizeY() ) ),
+				ValidationErrors );
 		}
 
 		FString Filename = Texture->AssetImportData->GetFirstFilename();
@@ -57,18 +63,31 @@ EDataValidationResult UBUIEditorValidator_Textures::ValidateLoadedAsset_Implemen
 
 		if ( EditorSettings.DataSourceFolder.Path.IsEmpty() )
 		{
-			ValidationErrors.Add( FText::FromString( "Data Source Folder is not set. Please set it in Editor Preferences." ) );
+			//ValidationErrors.Add( FText::FromString( "Data Source Folder is not set. Please set it in Editor Preferences." ) );
+			bAnyFailed = true;
+			AssetFails( InAsset, LOCTEXT( "BUIValidatorError_NoDataSourceFolder",
+				"Data Source Folder is not set. Please set it in Editor Preferences." ),
+				ValidationErrors );
 		}
 		else if ( !Filename.StartsWith( EditorSettings.DataSourceFolder.Path ) )
 		{
-			ValidationErrors.Add( FText::FromString( FString::Printf(
-				TEXT( "Importing a file from '%s', outside of Data Source Folder '%s'" ),
-				*Filename,
-				*EditorSettings.DataSourceFolder.Path ) ) );
+			//ValidationErrors.Add( FText::FromString( FString::Printf(
+				//TEXT( "Importing a file from '%s', outside of Data Source Folder '%s'" ),
+				//*Filename,
+				//*EditorSettings.DataSourceFolder.Path ) ) );
+			bAnyFailed = true;
+			AssetFails( InAsset, FText::Format( LOCTEXT( "BUIValidatorError_FileImportedOutsideDataSourceFolder", "Importing a file from '{0}', outside of Data Source Folder '{1}'" ),
+				FText::FromString( Filename ),
+				FText::FromString( EditorSettings.DataSourceFolder.Path ) ), ValidationErrors );
 		}
+		if ( !bAnyFailed )
+		{
+			AssetPasses( InAsset );
+		}
+		return bAnyFailed ? EDataValidationResult::Invalid : EDataValidationResult::Valid;
 	}
 
-	return ValidationErrors.Num() > 0 ? EDataValidationResult::Invalid : EDataValidationResult::Valid;
+	return EDataValidationResult::NotValidated;
 }
 
 #undef LOCTEXT_NAMESPACE
