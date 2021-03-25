68bb2aa6015bd7d24cc3473497949f464b887744
ben <ben@benui.ca>
2021-03-24 21:27:10 -0700

    Removed old comment

--- Source/BUIValidator/Public/BUIEditorValidator_Textures.h
+++ Source/BUIValidator/Public/BUIEditorValidator_Textures.h
@@ -3,10 +3,6 @@
 #include "EditorValidatorBase.h"
 #include "BUIEditorValidator_Textures.generated.h"
 
-/*
-* Validates that localized assets (within the L10N folder) conform to a corresponding source asset of the correct type.
-* Localized assets that fail this validation will never be loaded as localized variants at runtime.
-*/
 UCLASS( meta = ( DisplayName = "BUI Texture Validator" ) )
 class BUIVALIDATOR_API UBUIEditorValidator_Textures : public UEditorValidatorBase
 {
